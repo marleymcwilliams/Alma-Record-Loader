44ed873966e6223c31511b8acb1454dc3f9ea9a2
marleymcwilliams <marley.mcwilliams@gmail.com>
2018-08-20 11:52:55 -0700

    Removing useless functions

--- renderer.js
+++ renderer.js
@@ -1,44 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-$(document).ready(function(){
-    var request = require('request');
-  var url = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/acq/funds';
-  var queryParams = '?' +  encodeURIComponent('limit') + '=' + encodeURIComponent('100')+ '&' +  encodeURIComponent('offset') + '=' + encodeURIComponent('0')+ '&' +  encodeURIComponent('library') + '=' + encodeURIComponent('Watzek')+ '&' +  encodeURIComponent('format') + '=' + encodeURIComponent('json')+ '&' +  encodeURIComponent('apikey') + '=' + encodeURIComponent('l7xxd4db206386cc41ddb45583cb2336aee6');
-  request({
-    url: url + queryParams,
-    method: 'GET'
-  }, function (error, response, body) {
-    var body2 = JSON.parse(body);
-    //console.dir(body2);
-    var select = ""
-      for (i = 0; i < body2.fund.length; ++i) {
-        var code = body2.fund[i].code
-        var name = body2.fund[i].name
-        var opt = "<option value='"+ code +"'>" + name + " " + "(" + code + ")" + "</option>"
-        select += opt
-      }
-    $("#fundcode").html(select);
-  });
-  if($('#isbnmode').is(':checked')) {
-    $('#input').attr('maxlength','13'); }
-  if($('#oclcmode').is(':checked')) {
-    $('#input').attr('maxlength','10');
-  }
-});
 
 $.fn.press = function() { //Submit function
 
-  $(".metadata").html("");
+  $(".metadata").html(""); //resets metadata
 
-  if($('#isbnmode').is(':checked')) {
+  if($('#isbnmode').is(':checked')) { //function for isbn input
     var isbn = $('#input').val().replace(/\s/g, '');
     check = false
     apcallisbn(isbn, check);
     $("#amazon").load("templates/amazon.html");
     $('#input').select(); }
 
-  if($('#oclcmode').is(':checked')) {
+  if($('#oclcmode').is(':checked')) { //function for oclc input
     var oclc = $('#input').val().replace(/\s/g, '');
     check = true
     apcalloclc(oclc, check);
@@ -58,7 +33,7 @@ $(document).keypress(function(e) { //Pressing "enter" key counts as submit
     }
 });
 
-function apcalloclc(oclc, check){ //All the crap submit does for an OCLC input
+function apcalloclc(oclc, check){ //Picking out information given OCLC input
   var request = require('request');
   var url = 'http://www.worldcat.org/webservices/catalog/content/' + oclc;
   var queryParams = '?servicelevel=full&' +  encodeURIComponent('wskey') + '=' + encodeURIComponent('uwpGfFREPIyE38NK4wmJATF53xA1E2qMbIM2ksm1ZPfxtXGVWEccdDb8qb1oqjF2rC85WWC4mQpMahuZ');
@@ -68,14 +43,15 @@ function apcalloclc(oclc, check){ //All the crap submit does for an OCLC input
       method: 'GET'
   },
   function (error, response, body) {
-      //console.log(body);
 
       var parseString = require('xml2js').parseString;
       parseString(body, function (err, result) {
           console.dir(result);
 
           if(result==undefined || !result.record.hasOwnProperty('datafield')){ //If it screws up
+
             $("#failureOCLC").css("display", "inline");
+
           }else{
             fields = result.record.datafield
 
@@ -84,65 +60,33 @@ function apcalloclc(oclc, check){ //All the crap submit does for an OCLC input
                 if(fields[i].$.tag == 20){ //tag 20 encodes isbn number
                   var isbn = fields[i].subfield[0]._
                   if(isbn.length == 13 && check == true){
-                    apcallisbn(isbn, check); }
-
-                  if(fields[i].subfield.length > 1){ //tag 20 also has a chance of encoding material type
-                    var Material = fields[i].subfield[1]._
-                    $("#Material").html("<b>Material Type: </b>" + Material); } }
-
+                    apcallisbn(isbn, check);
+                  }
+                }
 
                 if(fields[i].$.tag == 100){ //tag 100 encodes author
                   var Author = fields[i].subfield[0]._
-                  $("#Author").html("<b>Author: </b>" + Author); }
-
-
-                  $("#oclc").html("<b>OCLC: </b>" + oclc); //sending oclc to amazon.html
-
-                  $("#link").html("<b>Additional Information: </b>" + 'http://worldcat.org/oclc/' + oclc); //encoding link to worldcat (given oclc)
-
+                }
 
                 if(fields[i].$.tag == 260){ //tag 260 encodes Publisher / Publication Location and Time
                   var PubPlace = fields[i].subfield[0]._
-                  $("#PubPlace").html("<b>Publication Location: </b>" + PubPlace);
-
                   var Pub = fields[i].subfield[fields[i].subfield.length - 2]._
-                  $("#Pub").html("<b>Publisher: </b>" + Pub);
-
                   var PubYear = fields[i].subfield[fields[i].subfield.length - 1]._
-                  $("#PubYear").html("<b>Publication Year: </b>" + PubYear); }
-
+                }
 
                 if(fields[i].$.tag == 264 && fields[i].$.ind2 == 1){ //tag 264 encodes Publisher / Publication Location and Time
                   var PubPlace = fields[i].subfield[0]._
-                  $("#PubPlace").html("<b>Publication Location: </b>" + PubPlace);
-
                   var Pub = fields[i].subfield[1]._
-                  $("#Pub").html("<b>Publisher: </b>" + Pub);
-
                   var PubYear = fields[i].subfield[2]._
-                  $("#PubYear").html("<b>Publication Year: </b>" + PubYear); }
-
+                }
 
                 if(fields[i].$.tag == 776 && fields[i].$.ind2 == 8){ //Tag 776 encodes Material and Publisher
                   var Material = fields[i].subfield[0]._
-                  $("#Material").html("<b>Material Type: </b>" + Material);
-
                   var Pub = fields[i].subfield[3]._
-                  $("#Pub").html("<b>Publisher: </b>" + Pub); }
+                }
 
-
-                if(fields[i].$.tag == 655){ //Tag 665 has a chance of encoding Material
-                  var Material2 = fields[i].subfield[0]._
-                  $("#Material2").html("<b>Second Material Type: </b>" + Material2); }
-
-
-                if(fields[i].$.tag == 588){ //Tag 588 has a chance of encoding Material
-                  var Material = fields[i].subfield[0]._
-                  $("#Material").html("<b>Material Type: </b>" + Material); }
-
-                localStorage.setItem("oclc", oclc);
+                localStorage.setItem("oclc", oclc); //Locally store all the important stuff
                 localStorage.setItem("Material", Material);
-                localStorage.setItem("Material2", Material2);
                 localStorage.setItem("PubPlace", PubPlace);
                 localStorage.setItem("Pub", Pub);
                 localStorage.setItem("PubYear", PubYear);
@@ -157,7 +101,7 @@ function apcalloclc(oclc, check){ //All the crap submit does for an OCLC input
   });
 }
 
-function apcallisbn(isbn, check){ //All the crap submit does for an ISBN input
+function apcallisbn(isbn, check){ //Picking out information given ISBN input
 
   var request = require('request');
   var url = 'http://www.worldcat.org/webservices/catalog/search/worldcat/opensearch?q=' + isbn;
@@ -168,45 +112,31 @@ function apcallisbn(isbn, check){ //All the crap submit does for an ISBN input
       method: 'GET'
   },
   function (error, response, body) {
-      //console.log(body);
 
       var parseString = require('xml2js').parseString;
       parseString(body, function (err, result) {
-          console.dir(result);  //Object > feed: > entry: Array(#) > blah
+          console.dir(result);  //log the info
 
         if(result==undefined || !result.feed.hasOwnProperty('entry')){ //If it screws up
-          $("#failureISBN").css("display", "inline");
-        }else{
-          fields = result.feed.entry[result.feed.entry.length - 1]
 
+          $("#failureISBN").css("display", "inline");
+
+        }else{
+
+          fields = result.feed.entry[result.feed.entry.length - 1]
 
           if(check == false){
             oclc = fields['oclcterms:recordIdentifier'][0] //Get the OCLC Number
-              $("#oclc").html("<b>OCLC: </b>" + oclc);
               apcalloclc(oclc, check); }
 
-
           Title = result.feed.entry[0].title[0] //Get the title
-            $("#Title").html("<b>Title: </b>" + Title);
-
-
-          Title2 = fields.title[0] //Sometimes titles are different for different versions
-            if(Title != Title2){
-              $("#Title2").html("<b>Second Title: </b>" + Title2); }
-
+          $("#Title").html("<b>Title: </b>" + Title);
 
           Author = fields.author[0].name[0] //Get the Author
-            $("#Author").html("<b>Author: </b>" + Author);
-
-            $("#isbbn").html("<b>ISBN: </b>" + isbn); //Print out the ISBN
-
-
           link = fields.link[0].$.href //Get a link to worldcat (for additional info)
-            $("#link").html("<b>Additional Information: </b>" + link);
 
           localStorage.setItem("isbn", isbn);
           localStorage.setItem("Title", Title);
-          localStorage.setItem("Title2", Title2);
           localStorage.setItem("Author", Author);
           localStorage.setItem("link", link);
 
@@ -220,12 +150,9 @@ function apcallisbn(isbn, check){ //All the crap submit does for an ISBN input
   });
 }
 
-function mother(){ //Main function which wraps everything together
-  information = createjson();
-/*  var jsonString = JSON.stringify(information);
-  console.dir(JSON.parse(jsonString));
-*/
-  console.log(
+function mother(){ //Sets up JSON string
+
+var almastring =
     {
       "owner": {
         "value": "Watzek"
@@ -238,24 +165,24 @@ function mother(){ //Main function which wraps everything together
       },
       "rush": false,
       "price": {
-        "sum": $('#cost').val(),
+        "sum": $("#cost").val(),
         "currency": {
           "value": "USD"
         }
       },
-      "url": information.link,
+      "url": localStorage.getItem("link"),
       "vendor_account": "wamaz",
       "vendor_reference_number": "",
       "po_number": "",
       "invoice_reference": "",
       "resource_metadata": {
-        "title": information.title,
-        "author": information.author,
+        "title": localStorage.getItem("Title"),
+        "author": localStorage.getItem("Author"),
         "issn": null,
-        "isbn": information.isbn,
-        "publisher": information.publisher,
-        "publication_place": information.pubplace,
-        "publication_year": information.pubyear,
+        "isbn": localStorage.getItem("isbn"),
+        "publisher": localStorage.getItem("Pub"),
+        "publication_place": localStorage.getItem("PubPlace"),
+        "publication_year": localStorage.getItem("PubYear"),
         "vendor_title_number": ""
       },
       "reporting_code": "BOOK",
@@ -264,33 +191,40 @@ function mother(){ //Main function which wraps everything together
       "material_type": {
         "value": "BOOK"
       },
-      "fund_distributions": {
-        "fund_distribution": {
-          "fund_code": $("#fundcode").val(),
-          "amount":{
-            "sum": $('#cost').val(),
+      "fund_distribution": [
+      {
+        "fund_code": {
+          "value": $("#fundcode").val(),
+        },
+        "amount":{
+            "sum": $("#cost").val(),
             "currency": {
               "value": "USD"
+            }
           }
-        }
       }
-    }
-  });
-  console.log(information.title);
+    ],
+  };
+  console.dir(almastring);
+
+  var almastring2 = JSON.stringify(almastring);
+  $("#almajson").html("<b>JSON String: </b>" + almastring2);
+
+  genesis(almastring2);
+
+  console.log(localStorage.getItem("Title"));
 }
 
-function createjson(){ //takes all the crap generated by apcall and makes a nice JSONstring
-  var information = new Object();
-    information.title = localStorage.getItem("Title");
-    information.title2 = localStorage.getItem("Title2");
-    information.author  = localStorage.getItem("Author");
-    information.oclc = localStorage.getItem("oclc");
-    information.isbn = localStorage.getItem("isbn");
-    information.publisher = localStorage.getItem("Pub");
-    information.pubplace = localStorage.getItem("PubPlace");
-    information.pubyear = localStorage.getItem("PubYear");
-    information.material = localStorage.getItem("Material");
-    information.material2 = localStorage.getItem("Material2");
-    information.link = localStorage.getItem("link");
-  return(information);
+function genesis(x){ //Sends JSON string to alma
+  var request = require('request');
+  var url = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/acq/po-lines';
+  var queryParams = '?' +  encodeURIComponent('apikey') + '=' + encodeURIComponent('l7xxd4db206386cc41ddb45583cb2336aee6');
+  request({
+      url: url + queryParams,
+      body: x,
+      headers: { 'Content-Type':'application/json'  },
+      method: 'POST'
+  }, function (error, response, body) {
+      console.log('Reponse received', body);
+  });
 }
